20b59fb04c89fe86c58993da07117200c21b755d
wyc1123 <137739645+wyc1123@users.noreply.github.com>
2023-07-18 17:07:11 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -170,9 +170,9 @@ const BLOG = {
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
-      name: '风を共に舞う気持ち',
+      name: '向云端',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
+      url: 'https://music.163.com/song/media/outer/url?id=2049512697.mp3',
       cover:
           'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
     },
